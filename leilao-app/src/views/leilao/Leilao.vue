74c392eb7ce67df2ed8d7aac0134684b5c3f9a5b
vitorssantos <vitorssantos@brbyte.com>
2024-03-11 19:56:53 -0400

    Tabela Leilão e generalizando componente TabelaPadrao

---
+++ leilao-app/src/views/leilao/Leilao.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		<TituloPagina titulo="Dados dos Leilões:" />
+		<TabelaPadrao
+			:headers="headers"
+			url="leilao"
+			acaoDisabled="true"
+		/>
+	</div>
+</template>
+
+<script>
+	import TituloPagina from '../../components/TituloPagina.vue';
+	import TabelaPadrao from '../../components/TabelaPadrao.vue';
+
+	export default {
+		name: 'Leilao',
+		data() {
+			return {
+				dialog: false,
+				headers: [
+					{ text: 'Vendedor', value: 'vendedor' },
+					{ text: 'Somatório base ($)', value: 'totalLeilao' },
+					{ text: 'Data de Início', value: 'inicioPrevisto' },
+				],
+			};
+		},
+		components: {
+			TituloPagina,
+			TabelaPadrao
+		}
+	};
+</script>
