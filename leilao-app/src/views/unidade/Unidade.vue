8d4d7e4400d46ff8b35cfe3013d416af804ebfaa
vitorssantos <vitorssantos@brbyte.com>
2024-03-18 00:20:11 -0400

    Unidade Files

--- leilao-app/src/views/unidade/Unidade.vue
+++ leilao-app/src/views/unidade/Unidade.vue
@@ -1,42 +1,174 @@
 <template>
-	<div>
-		<Snackbar
-			cor="success"
-			texto="Unidade atualizada com sucesso!"
-			icon="mdi-checkbox-marked-circle"
-			:ativo="snackbar"
-		/>
-		<TituloPagina titulo="Dados da Unidade:" />
-		<TabelaEditavel
-			:headers="headers"
-			url="unidade"
-		/>
-	</div>
+	<v-data-table
+		:headers="headers"
+		:items="unidadeDataTimeFormat"
+		sort-by="id"
+		class="elevation-1"
+	>
+		<template v-slot:top>
+			<v-toolbar flat>
+				<v-toolbar-title>Unidades</v-toolbar-title>
+				<v-divider
+					class="mx-4"
+					inset
+					vertical
+				></v-divider>
+				<v-spacer></v-spacer>
+				<template>
+					<v-btn
+						color="primary"
+						dark
+						class="mb-2"
+						@click="openEditDialogNew"
+					>
+						<v-icon class="mr-2">mdi-plus-circle</v-icon>
+						Nova Unidade
+					</v-btn>
+				</template>
+
+				<unidade-dialog-edit
+					:editedUnidade="editedUnidade"
+					@save="saveEditDialog"
+					@close="closeDialog"
+					ref="dialogEdit"
+				/>
+
+				<dialog-delete
+					:titulo="`Unidade '${editedUnidade.nome}'`"
+					@deleteItem="deleteUnidade"
+					@close="closeDialog"
+					ref="dialogDelete"
+				/>
+			</v-toolbar>
+		</template>
+		<template v-slot:item.actions="{ item }">
+			<v-icon
+				small
+				class="mr-2"
+				@click="openEditDialog(item)"
+			>
+				mdi-pencil
+			</v-icon>
+			<v-icon
+				small
+				@click="openDeleteDialog(item)"
+			>
+				mdi-delete
+			</v-icon>
+		</template>
+	</v-data-table>
 </template>
 
 <script>
-	import Snackbar from '../../components/Snackbar.vue';
-	import TituloPagina from '../../components/TituloPagina.vue';
-	import TabelaEditavel from '../../components/TabelaEditavel.vue';
+	import { mapState } from 'vuex';
+	import colors from '../../assets/colors.js';
+	import UnidadeDialogEdit from './components/UnidadeDialogEdit';
+	import DialogDelete from '../../components/DialogDelete';
 
 	export default {
-		name: 'Unidade',
-		data() {
-			return {
-				headers: [
-					{ text: '#', value: 'id' },
-					{ text: 'Nome', value: 'nome' },
-					{ text: 'Atualizado', value: 'updatedAt' },
-					{ text: 'Criado', value: 'createdAt' },
-					{ text: 'Ações', value: 'actions' },
-				],
-				snackbar: false,
-			};
+		data: () => ({
+			corVinho: colors.vinho,
+			corCinza: colors.cinza,
+			corLaranja: colors.laranja,
+			headers: [
+				{ text: '#', value: 'id' },
+				{ text: 'Nome', value: 'nome' },
+				{ text: 'Atualizado', value: 'updatedAt' },
+				{ text: 'Criado', value: 'createdAt' },
+				{ text: 'Ações', value: 'actions', sortable: false },
+			],
+			editedId: -1,
+			editedUnidade: {
+				nome: '',
+			},
+			defaultItem: {
+				nome: '',
+			},
+		}),
+
+		computed: {
+			...mapState(['unidadeData']),
+			unidadeDataTimeFormat() {
+				return this.unidadeData.map(unidade => ({
+					...unidade,
+					createdAt: unidade.createdAt.split('T')[0],
+					updatedAt: unidade.updatedAt.split('T')[0],
+				}));
+			},
+		},
+		created() {
+			this.fetchData();
+		},
+		methods: {
+			async fetchData() {
+				await this.$store.dispatch('fetchData', 'unidade');
+			},
+
+			openEditDialogNew() {
+				this.$refs.dialogEdit.open();
+				this.editedId = -1;
+			},
+
+			openEditDialog(item) {
+				this.$refs.dialogEdit.open();
+				this.editedId = item.id;
+				this.editedUnidade = Object.assign({}, item);
+			},
+
+			async saveEditDialog() {
+				try {
+					if (this.editedId > -1) {
+						const corpo = {
+							nome: this.editedUnidade.nome,
+						};
+						await this.$store.dispatch('updateData', {
+							endpoint: 'unidade',
+							id: this.editedId,
+							corpo: corpo,
+						});
+					} else {
+						const corpo = {
+							nome: this.editedUnidade.nome,
+						};
+						await this.$store.dispatch('saveNewData', {
+							endpoint: 'unidade',
+							corpo: corpo,
+						});
+					}
+					await this.fetchData();
+				} catch (error) {
+					console.log('error: ', error);
+				}
+			},
+
+			openDeleteDialog(item) {
+				this.$refs.dialogDelete.open();
+				this.editedId = item.id;
+				this.editedUnidade = Object.assign({}, item);
+			},
+
+			closeDialog() {
+				this.$nextTick(() => {
+					this.editedUnidade = Object.assign({}, this.defaultItem);
+					this.editedId = -1;
+				});
+			},
+
+			async deleteUnidade() {
+				try {
+					await this.$store.dispatch('deleteData', {
+						endpoint: 'unidade',
+						id: this.editedId,
+					});
+					await this.fetchData();
+				} catch (error) {
+					console.log('error: ', error);
+				}
+			},
 		},
 		components: {
-			TituloPagina,
-			Snackbar,
-			TabelaEditavel,
+			UnidadeDialogEdit,
+			DialogDelete,
 		},
 	};
 </script>
---
+++ leilao-app/src/views/unidade/components/UnidadeDialogEdit.vue
@@ -0,0 +1,77 @@
+<template>
+	<v-dialog
+		v-model="dialog"
+		max-width="500px"
+	>
+		<v-card>
+			<v-card-title>
+				<span class="text-h5">{{ formTitle }}</span>
+			</v-card-title>
+
+			<v-card-text>
+				<v-container>
+					<v-row>
+						<v-col
+							cols="12"
+							sm="12"
+							md="12"
+						>
+							<v-text-field
+								v-model="editedUnidade.nome"
+								label="Nome da Unidade"
+								maxlength="128"
+								counter
+							></v-text-field>
+						</v-col>
+					</v-row>
+				</v-container>
+			</v-card-text>
+
+			<v-card-actions>
+				<v-spacer></v-spacer>
+				<v-btn
+					color="blue darken-1"
+					text
+					@click="close"
+				>
+					Cancel
+				</v-btn>
+				<v-btn
+					color="blue darken-1"
+					text
+					@click="save"
+				>
+					Save
+				</v-btn>
+			</v-card-actions>
+		</v-card>
+	</v-dialog>
+</template>
+
+<script>
+	export default {
+		name: 'UnidadeDialogEdit',
+		props: ['editedUnidade'],
+		data: () => ({
+			dialog: false,
+		}),
+		methods: {
+			save() {
+				this.dialog = false;
+				this.$emit('save');
+			},
+			close() {
+				this.dialog = false;
+				this.$emit('close');
+			},
+			open() {
+				this.dialog = true;
+			},
+		},
+		computed: {
+			formTitle() {
+				return this.editedId === -1 ? 'Nova Unidade' : 'Editar Unidade';
+			},
+		},
+	};
+</script>
