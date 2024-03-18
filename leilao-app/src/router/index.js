e5b1bf9e38c2ac53a3eb095506b13ff93be83844
vitorssantos <vitorssantos@brbyte.com>
2024-03-18 00:19:41 -0400

    index router e store

--- leilao-app/src/router/index.js
+++ leilao-app/src/router/index.js
@@ -1,38 +1,38 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: () => import('../views/home/Home.vue')
-  },
-  {
-    path: '/empresas',
-    name: 'Empresa',
-    component: () => import('../views/empresa/Empresa.vue')
-  },
-  {
-    path: '/empresa/:id',
-    name: 'EmpresaItem',
-    component: () => import('../views/empresa/EmpresaItem.vue')
-  },
-  {
-    path: '/leiloes',
-    name: 'Leilao',
-    component: () => import('../views/leilao/Leilao.vue')
-  },
-  {
-    path: '/unidades',
-    name: 'Unidade',
-    component: () => import('../views/unidade/Unidade.vue')
-  }
-]
+	{
+		path: '/',
+		name: 'Home',
+		component: () => import('../views/home/Home.vue'),
+	},
+	{
+		path: '/empresas',
+		name: 'Empresa',
+		component: () => import('../views/empresa/Empresa.vue'),
+	},
+	{
+		path: '/empresa/:id',
+		name: 'EmpresaItem',
+		component: () => import('../views/empresa/EmpresaItem.vue'),
+	},
+	{
+		path: '/leiloes',
+		name: 'Leilao',
+		component: () => import('../views/leilao/Leilao.vue'),
+	},
+	{
+		path: '/unidades',
+		name: 'Unidade',
+		component: () => import('../views/unidade/Unidade.vue'),
+	},
+];
 
 const router = new VueRouter({
-  routes
-})
+	routes,
+});
 
-export default router
+export default router;
--- leilao-app/src/store/index.js
+++ leilao-app/src/store/index.js
@@ -9,6 +9,7 @@ export default new Vuex.Store({
 		empresaData: [],
 		unidadeData: [],
 		leilaoData: [],
+		empresaItemData: {},
 	},
 	mutations: {
 		setEmpresaData(state, list) {
@@ -19,33 +20,88 @@ export default new Vuex.Store({
 		},
 		setLeilaoData(state, list) {
 			state.leilaoData = list;
-		}
+		},
+		setEmpresaItemData(state, list) {
+			state.empresaItemData = list;
+		},
 	},
 	actions: {
 		async fetchData(context, endpoint) {
 			try {
 				const response = await axios.get('http://localhost:8081/' + endpoint);
 				const list = response.data;
-				context.commit('set' + endpoint.charAt(0).toUpperCase() + endpoint.slice(1) + 'Data', list);
+				context.commit(
+					'set' + endpoint.charAt(0).toUpperCase() + endpoint.slice(1) + 'Data',
+					list
+				);
 			} catch (error) {
 				console.error('Erro ao buscar dados da API:', error);
 			}
-		}
+		},
+		async fetchDataById(context, { endpoint, id }) {
+			try {
+				const response = await axios.get(
+					`http://localhost:8081/${endpoint}/${id}`
+				);
+				const list = response.data;
+				context.commit(
+					'set' +
+						endpoint.charAt(0).toUpperCase() +
+						endpoint.slice(1) +
+						'ItemData',
+					list
+				);
+			} catch (error) {
+				console.error('Erro ao buscar dados da API:', error);
+			}
+		},
+		async updateData(context, { endpoint, id, corpo }) {
+			try {
+				const response = await axios.put(
+					`http://localhost:8081/${endpoint}/${id}`,
+					corpo
+				);
+				return response;
+			} catch (error) {
+				console.error('Erro ao enviar requisição:', error);
+			}
+		},
+		async saveNewData(context, { endpoint, corpo }) {
+			try {
+				const response = await axios.post(
+					`http://localhost:8081/${endpoint}`,
+					corpo
+				);
+				return response;
+			} catch (error) {
+				console.error('Erro ao enviar requisição:', error);
+			}
+		},
+		async deleteData(context, { endpoint, id }) {
+			try {
+				const response = await axios.delete(
+					`http://localhost:8081/${endpoint}/${id}`
+				);
+				return response;
+			} catch (error) {
+				console.error('Erro ao enviar requisição:', error);
+			}
+		},
 	},
 	getters: {
-    empresaCounter (state) {
-      return state.empresaData.length
-    },
-		unidadeCounter (state) {
-      return state.unidadeData.length
-    },
-		leilaoTotalValue (state) {
+		empresaCounter(state) {
+			return state.empresaData.length;
+		},
+		unidadeCounter(state) {
+			return state.unidadeData.length;
+		},
+		leilaoTotalValue(state) {
 			let total = 0;
 			for (const leilao of state.leilaoData) {
 				total += leilao.totalLeilao;
-			}	
-      return parseFloat(total);
-    }
-  },
+			}
+			return parseFloat(total);
+		},
+	},
 	modules: {},
 });
